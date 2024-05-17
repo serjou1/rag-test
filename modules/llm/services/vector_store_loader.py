52c69c876ba2adcd5015df0f633f30400fc0bf6e
serjou <serhii.nikitin@remme.io>
2024-05-17 15:47:27 +0300

    save vectors to local db

--- modules/llm/services/vector_store_loader.py
+++ modules/llm/services/vector_store_loader.py
@@ -8,7 +8,12 @@ from modules.llm.services.chunks_loader import get_chunks
 def initialize_vector_store_retriever() -> VectorStoreRetriever:
     chunks = get_chunks()
 
-    vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=FastEmbedEmbeddings(),
+        persist_directory="./vstore"
+    )
+    vector_store.persist()
     retriever = vector_store.as_retriever()
 
     return retriever
